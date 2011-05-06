6b2977d0f495e9f7c8b21d055f96881b8643aabd
liukaixuan <liukaixuan@fb99edf0-f902-11de-8591-91bc733d3be8>
2011-05-06 03:12:57 +0000

    isDemon checking changed to Thread.isDemon() ExecuteService is exported to be injected.

--- trunk/core/org/guzz/service/core/impl/DebugServiceImpl.java
+++ trunk/core/org/guzz/service/core/impl/DebugServiceImpl.java
@@ -73,10 +73,13 @@ public class DebugServiceImpl extends AbstractService implements DebugService, S
 	
 	protected boolean isDemonThread(){
 		Thread t = Thread.currentThread() ;
-		String name =t.getName() ;
-		if(name == null) return false ;
 		
-		return name.startsWith(DebugService.DEMON_NAME_PREFIX) ;
+		return t.isDaemon() ;
+		
+//		String name =t.getName() ;
+//		if(name == null) return false ;
+//		
+//		return name.startsWith(DebugService.DEMON_NAME_PREFIX) ;
 	}
 	
 	public void logSQL(String sql, long nanoTime){
